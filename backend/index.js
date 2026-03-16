2cf55497260b5172ef67607456ebc63cfd402d7b
BhawnaNautiyalll <bhananautiyalll@gmail.com>
2026-03-16 07:31:14 +0530

    fix cors for vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://hackathonmate.vercel.app"
+    "https://hackathon-mate.vercel.app"
   ],
   credentials: true
 }));
